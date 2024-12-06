b056226ccf14f129631c94c3692648ab73aa3515
Jesus Castro <castrojesus39@gmail.com>
2024-12-05 16:29:55 -0800

    model class of the Receipt

---
+++ Models/Receipt.cs
@@ -0,0 +1,16 @@
+﻿namespace Receipt_Processor.Models;
+
+public class Receipt
+{
+    public string Retailer { get; set; }
+    public string PurchaseDate { get; set; }
+    public string PurchaseTime { get; set; }
+    public List<Item> Items { get; set; }
+    public int Total { get; set; }
+}
+
+public class Item
+{
+    public string ShortDescription { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Models/ReceiptDatabase.cs
@@ -0,0 +1,6 @@
+﻿namespace Receipt_Processor.Models;
+
+public class ReceiptDatabase
+{
+    private static List<Receipt> Receipt = new List<Receipt>() { };
+}
