4a4083a67a657fa646963e985fc6922f8a99773f
Jesus Castro <castrojesus39@gmail.com>
2024-12-06 20:43:10 -0800

    Controller logic created for both POST and GET with proper methods

--- Controllers/ReceiptController.cs
+++ Controllers/ReceiptController.cs
@@ -1,8 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Receipt_Processor.Models;
 
 namespace Receipt_Processor.Controllers;
 
-public class ReceiptController : ControllerBase
+[ApiController]
+[Route("[controller]")]
+public class ReceiptController() : ControllerBase
 {
+    [HttpPost]
+    [Route("process")]
+    public ActionResult<Guid> CreateIdFromReceipt([FromBody] Receipt receipt)
+    {
+        receipt.id = Guid.NewGuid();
+        ReceiptDatabase.ReceiptDB.Add(receipt);
+
+        return Ok(receipt.id);
+    }
     
+    [HttpGet("{id}")]
+    public ActionResult<List<Receipt>> GetAllReceipts(Guid id)
+    {
+        var item = ReceiptDatabase.ReceiptDB.FirstOrDefault(i => i.id == id);
+
+        if (item == null)
+        {
+            return NotFound($"Item with ID {id} not found");
+        }
+
+        return Ok(item);
+    }
 }
