bc1dcf197d1e9b9593683bdcb182ef9212d394ca
Jesus Castro <castrojesus39@gmail.com>
2024-12-08 19:51:08 -0800

    added ReceiptService/IReceiptService class via DI to support logic for finding out the amount of points using ID

--- Controllers/ReceiptController.cs
+++ Controllers/ReceiptController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Receipt_Processor.Models;
+using Receipt_Processor.Services;
 
 namespace Receipt_Processor.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ReceiptController() : ControllerBase
+public class ReceiptController(IReceiptService receiptService) : ControllerBase
 {
+    // DI ReceiptService class
+    private readonly IReceiptService _receiptService = receiptService;
+    
     [HttpPost]
     [Route("process")]
     public ActionResult<Guid> CreateIdFromReceipt([FromBody] Receipt receipt)
@@ -17,16 +21,32 @@ public class ReceiptController() : ControllerBase
         return Ok(receipt.id);
     }
     
-    [HttpGet("{id}")]
-    public ActionResult<List<Receipt>> GetAllReceipts(Guid id)
+    // create the logic to process the points
+    // response is going to be a json object called "points"
+    [HttpGet("{id}/points")]
+    public ActionResult<List<Receipt>> GetPointsById(Guid id)
     {
         var item = ReceiptDatabase.ReceiptDB.FirstOrDefault(i => i.id == id);
-
+        
         if (item == null)
         {
             return NotFound($"Item with ID {id} not found");
         }
 
-        return Ok(item);
+        var result = _receiptService.checkAlphanumeric(item);
+        
+        return Ok(new {points = result});
     }
+    
+    // [HttpGet]
+    // [Route("test")]
+    // public ActionResult GetPointsById()
+    // {
+    //     int num1 = 2;
+    //     int num2 = 2;
+    //
+    //     int sum = num1 + num2;
+    //     var data = new { sum = sum };
+    //     return Ok(data);
+    // }
 }
